64ca0ba41db59b4d50c4d2b5b31da0245e91fa56
arsrsyh <gotrany97@gmail.com>
2023-05-23 22:25:37 +0800

    Login

---
+++ src/pages/Login.jsx
@@ -0,0 +1,14 @@
+// import React from 'react'
+
+export default function SingIn() {
+    return (
+        <div>
+            <section>
+
+            </section>
+            <section>
+                <span>Login</span>
+            </section>
+        </div>
+    )
+}
