fc0a5d3602763122d3b028cba07fbbf545280439
Gunadi PS <gndsulistiawan@gmail.com>
2023-05-25 14:48:53 +0700

    create: slicing notification page

--- src/pages/ArticleView.jsx
+++ src/pages/ArticleView.jsx
@@ -4,7 +4,6 @@ import { AiOutlineLike, AiOutlineFieldTime } from 'react-icons/ai';
 import { RiBookmarkFill } from 'react-icons/ri';
 import { Link } from 'react-router-dom';
 
-import categoryPict from '../assets/image/category-pict.png';
 import articleImage from '../assets/image/article-image.png';
 import { IoChevronBackOutline } from 'react-icons/io5';
 
@@ -72,16 +71,16 @@ const ArticleView = () => {
                         <div className="px-7 md:px-16 lg:px-24 xl:px-28">
                             <div className="flex flex-wrap items-center justify-center gap-7 h-full ">
                                 During the 2019 Southeast Asian Games, governor the Sports Authority of Thailand (SAT) Kongsak Yodmanee criticised the organization of the games, as the Philippines held the games in many cities and
-                                municipalities, causing to the various concerns and controversies. He will propose to hold the next Thailand's Southeast Asian Games in "one" city or province. He also suggested Bangkok and Chonburi Province
-                                are the best choice for hosting the Thailand's games. He mentioned Bangkok traffic is less congested than Manila and the city has many existing venues for the games but water sports venues. Bangkok hosted the
+                                municipalities, causing to the various concerns and controversies. He will propose to hold the next Thailands Southeast Asian Games in one city or province. He also suggested Bangkok and Chonburi Province are
+                                the best choice for hosting the Thailands games. He mentioned Bangkok traffic is less congested than Manila and the city has many existing venues for the games but water sports venues. Bangkok hosted the
                                 inaugural games in 1959 and 1967 as Southeast Asian Peninsular Games, which were the precursor to the modern Southeast Asian Games, and 1985 as Southeast Asian Games. Bangkok hosted many global and
                                 continental events such as four-time Asian Games and Summer Universiade in 2007. Bangkok will host the 2021 Asian Indoor and Martial Arts Games with Chonburi Province It acted as the test event and a prelude
                                 for the future multi-sport event, a proposed Youth Olympic Games in 2026. During the 2019 Southeast Asian Games, governor the Sports Authority of Thailand (SAT) Kongsak Yodmanee criticised the organization of
-                                the games, as the Philippines held the games in many cities and municipalities, causing to the various concerns and controversies. He will propose to hold the next Thailand's Southeast Asian Games in "one"
-                                city or province. He also suggested Bangkok and Chonburi Province are the best choice for hosting the Thailand's games. He mentioned Bangkok traffic is less congested than Manila and the city has many
-                                existing venues for the games but water sports venues. Bangkok hosted the inaugural games in 1959 and 1967 as Southeast Asian Peninsular Games, which were the precursor to the modern Southeast Asian Games,
-                                and 1985 as Southeast Asian Games. Bangkok hosted many global and continental events such as four-time Asian Games and Summer Universiade in 2007. Bangkok will host the 2021 Asian Indoor and Martial Arts
-                                Games with Chonburi Province It acted as the test event and a prelude for the future multi-sport event, a proposed Youth Olympic Games in 2026.
+                                the games, as the Philippines held the games in many cities and municipalities, causing to the various concerns and controversies. He will propose to hold the next Thailands Southeast Asian Games in one city
+                                or province. He also suggested Bangkok and Chonburi Province are the best choice for hosting the Thailands games. He mentioned Bangkok traffic is less congested than Manila and the city has many existing
+                                venues for the games but water sports venues. Bangkok hosted the inaugural games in 1959 and 1967 as Southeast Asian Peninsular Games, which were the precursor to the modern Southeast Asian Games, and 1985 as
+                                Southeast Asian Games. Bangkok hosted many global and continental events such as four-time Asian Games and Summer Universiade in 2007. Bangkok will host the 2021 Asian Indoor and Martial Arts Games with
+                                Chonburi Province It acted as the test event and a prelude for the future multi-sport event, a proposed Youth Olympic Games in 2026.
                             </div>
                         </div>
                     </div>
